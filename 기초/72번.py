67ee06691fb5c93a16f12839e382b4e9ea3c8f5f
Park HyunJun <162002859+Hyunjun-study@users.noreply.github.com>
2025-01-27 00:02:26 +0900

    Update 72번.py

--- 기초/72번.py
+++ 기초/72번.py
@@ -1,11 +1,15 @@
 def solution(str_list):
-    answer = []
-    if "l" not in str_list or "r" not in str_list:
+    if "l" not in str_list and "r" not in str_list:
         return []
-    elif str_list.index("l") < str_list.index("r"):
-        for i in range(str_list.index("l")):
-            answer.append(str_list[i])
-    elif str_list.index("l") > str_list.index("r"):
-        for i in range(str_list.index("r")+1,len(str_list)):
-            answer.append(str_list[i])
-    return answer
+    
+    if "l" in str_list and "r" in str_list:
+        if str_list.index("l") < str_list.index("r"):
+            return str_list[:str_list.index("l")]
+        else:
+            return str_list[str_list.index("r") + 1:]
+    
+    if "l" in str_list:
+        return str_list[:str_list.index("l")]
+    
+    if "r" in str_list:
+        return str_list[str_list.index("r") + 1:]
